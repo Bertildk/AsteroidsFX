172cc2ec3195fe26ad55d079ba4c70440f4e07bc
MarkusKaas23 <143800614+MarkusKaas23@users.noreply.github.com>
2025-05-06 10:40:31 +0200

    Revert "Tilføjet JUnit tests"
    This reverts commit 916b68eb46a98df5e1ef42c5aa04c9db14a3d9a4.

--- Common/src/main/java/dk/sdu/mmmi/cbse/common/data/GameData.java
+++ Common/src/main/java/dk/sdu/mmmi/cbse/common/data/GameData.java
@@ -4,7 +4,7 @@ public class GameData {
 
     private int displayWidth  = 800 ;
     private int displayHeight = 800;
-    private GameKeys keys = new GameKeys();
+    private final GameKeys keys = new GameKeys();
 
 
     public GameKeys getKeys() {
@@ -28,7 +28,4 @@ public class GameData {
     }
 
 
-    public void setKeys(GameKeys keys) {
-        this.keys = keys;
-    }
 }
